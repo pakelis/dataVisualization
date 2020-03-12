c43a015df18f27dba9784f0a6d2705fdf03b540e
root <root@DESKTOP-T2NHK0E.localdomain>
2020-03-12 21:35:20 +0200

    initial commit

--- Server/main/db.js
+++ Server/main/db.js
@@ -1,11 +1,11 @@
-const { Pool } = require("pg");
+const {Pool} = require('pg')
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "",
-  port: "5432"
-});
+  user: 'postgres',
+  host: 'localhost',
+  database: 'data_visualization',
+  password: '',
+  port: '5432',
+})
 
-module.exports = pool;
+module.exports = pool
--- Server/main/routes.js
+++ Server/main/routes.js
@@ -1,32 +1,32 @@
 //All our routes
-var express = require("express");
-var router = express.Router();
-var pool = require("./db");
+var express = require('express')
+var router = express.Router()
+var pool = require('./db')
 
 /*
   TEST ROUTE
 */
 
-router.get("/api/hello", (req, res) => {
+router.get('/api/hello', (req, res) => {
   // test route for our Client
-  res.json("hello world");
-});
+  res.json('hello world')
+})
 
 /*
    ROUTES SECTION
 */
 
-router.get("/api/get/tableone", (req, res, next) => {
-  pool.query(`SELECT * FROM renginiu_finansavimas`, (q_err, q_res) => {
-    res.json(q_res);
-    console.log(q_res);
-  });
-});
+router.get('/api/get/tableone', (req, res, next) => {
+  pool.query(`SELECT * FROM finance`, (q_err, q_res) => {
+    console.log(res.json(q_res.rows))
+    res.json(q_res.rows)
+  })
+})
 
-router.get("/api/get/allusers", (req, res, next) => {
+router.get('/api/get/allusers', (req, res, next) => {
   pool.query(`SELECT * FROM users`, (q_err, q_res) => {
-    res.json(q_res);
-  });
-});
+    res.json(q_res)
+  })
+})
 
-module.exports = router;
+module.exports = router
