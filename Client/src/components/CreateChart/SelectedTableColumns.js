4efd96cfbd20ce016881906756dda0ee04f6220b
pakelis <pakelis47@gmail.com>
2020-04-02 19:15:43 +0300

    initial commit

--- Client/src/components/CreateChart/SelectedTableColumns.js
+++ Client/src/components/CreateChart/SelectedTableColumns.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import { useAuth0 } from "../../react-auth0-spa";
-import { useSelectedTableValue } from "../../context";
+import React, {useState, useEffect} from 'react'
+import {useAuth0} from '../../react-auth0-spa'
+import {useSelectedTableValue} from '../../context'
 
 //libs
-import axios from "axios";
+import axios from 'axios'
 
 //antd
-import { List, Typography } from "antd";
-import IndicatorSelect from "./IndicatorSelect";
+import {List, Typography} from 'antd'
+import IndicatorSelect from './IndicatorSelect'
 
 const SelectedTableColumns = ({
   setIndicator,
@@ -17,48 +17,48 @@ const SelectedTableColumns = ({
   chartType,
   setChartType,
   handlePreview,
-  handleNames
+  handleNames,
 }) => {
-  const [data, setData] = useState();
-  const [selectedTableColumns, setSelectedTableColumns] = useState();
-  const { getTokenSilently } = useAuth0();
-  const { selectedTable } = useSelectedTableValue();
+  const [data, setData] = useState()
+  const [selectedTableColumns, setSelectedTableColumns] = useState()
+  const {getTokenSilently} = useAuth0()
+  const {selectedTable} = useSelectedTableValue()
 
   const getTableColums = async () => {
-    const token = await getTokenSilently();
+    const token = await getTokenSilently()
 
     let res = axios
-      .get("/admin/api/tablecolumns", {
+      .get('/admin/api/tablecolumns', {
         params: {
-          tableName: selectedTable
+          tableName: selectedTable,
         },
         headers: {
-          authorization: `Bearer ${token}`
-        }
+          authorization: `Bearer ${token}`,
+        },
       })
       .then(res => {
-        let columnNames = res.data.map(row => row.column_name);
-        handleNames(columnNames);
-        setData(res.data);
+        let columnNames = res.data.map(row => row.column_name)
+        handleNames(columnNames)
+        setData(res.data)
       })
-      .catch(err => console.log(err));
-  };
+      .catch(err => console.log(err))
+  }
 
   useEffect(() => {
     if (selectedTable != null) {
-      getTableColums();
+      getTableColums()
     }
-  }, [selectedTable]);
+  }, [selectedTable])
 
   return (
     <>
       <List
         size="large"
-        header={selectedTable != "" ? <div>{selectedTable}</div> : null}
+        header={selectedTable !== '' ? <div>{selectedTable}</div> : null}
         bordered
         dataSource={data}
         renderItem={item => <List.Item>{item.column_name}</List.Item>}
-        locale={{ emptyText: "You haven't selected any table yet" }}
+        locale={{emptyText: "You haven't selected any table yet"}}
       ></List>
       {data && (
         <IndicatorSelect
@@ -73,7 +73,7 @@ const SelectedTableColumns = ({
         />
       )}
     </>
-  );
-};
+  )
+}
 
-export default SelectedTableColumns;
+export default SelectedTableColumns
