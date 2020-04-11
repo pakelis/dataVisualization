8f35b9f20face7b95f8d93250f694f924599a789
pakelis <pakelis47@gmail.com>
2020-04-12 01:25:48 +0300

    adding new arc select for pie chart

--- Client/src/components/CreateChart/Diagrams/Piechart.js
+++ Client/src/components/CreateChart/Diagrams/Piechart.js
@@ -1,33 +1,33 @@
-import React from "react";
+import React from 'react'
 
 //react-responsive
-import { useMediaQuery } from "react-responsive";
+import {useMediaQuery} from 'react-responsive'
 
 //recharts
-import { Tooltip, Legend, ResponsiveContainer, PieChart, Pie } from "recharts";
+import {Tooltip, Legend, ResponsiveContainer, PieChart, Pie} from 'recharts'
 
-const Piechart = ({ chartData, indicator, chartNameField }) => {
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
-  const isDesktopOrLaptop = useMediaQuery({ query: "(min-width: 1224px)" });
+const Piechart = ({chartData, indicator, chartNameField}) => {
+  const isTabletOrMobile = useMediaQuery({query: '(max-width: 1224px)'})
+  const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'})
 
   return (
-    <ResponsiveContainer width="95%" height={750}>
-      <PieChart width={400} height={400}>
+    <ResponsiveContainer width={250} height={250}>
+      <PieChart width={250} height={250}>
         <Pie
-          data={chartData}
+          data={[chartData]}
           dataKey={indicator}
           nameKey="name"
-          cx="50%"
-          cy="50%"
+          // cx="50%"
+          // cy="50%"
           outerRadius={80}
           fill="#1DA57A"
           isAnimationActive={false}
         />
-        <Legend />
         <Tooltip />
+        <Legend />
       </PieChart>
     </ResponsiveContainer>
-  );
-};
+  )
+}
 
-export default Piechart;
+export default Piechart
