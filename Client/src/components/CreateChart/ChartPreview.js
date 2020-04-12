7a3daf663ffb58276cca36c9f2b8973e0271db37
pakelis <pakelis47@gmail.com>
2020-04-13 01:11:28 +0300

    fixed pie chart

--- Client/src/components/CreateChart/ChartPreview.js
+++ Client/src/components/CreateChart/ChartPreview.js
@@ -1,55 +1,50 @@
-import React, { useState, useEffect } from "react";
-import { useAuth0 } from "../../react-auth0-spa";
-import Barchart from "./Diagrams/Barchart";
-import Columnchart from "./Diagrams/Columnchart";
-import Piechart from "./Diagrams/Piechart";
+import React, {useState, useEffect} from 'react'
+import {useAuth0} from '../../react-auth0-spa'
+import Barchart from './Diagrams/Barchart'
+import Columnchart from './Diagrams/Columnchart'
+import Piechart from './Diagrams/Piechart'
 
 //moment
-import moment from "moment";
+import moment from 'moment'
 
 //ant-d
-import { Col, Row } from "antd";
+import {Col, Row} from 'antd'
 
 //react-responsive
-import { useMediaQuery } from "react-responsive";
+import {useMediaQuery} from 'react-responsive'
 
 //libs
-import axios from "axios";
+import axios from 'axios'
 
 //context
-import { useSelectedTableValue } from "../../context";
+import {useSelectedTableValue} from '../../context'
 
 //TODO fix custom tool tip!!!!
 
-const ChartPreview = ({
-  indicator,
-  chartNameField,
-  chartType,
-  tableColumns,
-}) => {
-  const { selectedTable } = useSelectedTableValue();
-  const { getTokenSilently } = useAuth0();
-  const [responseData, setResponseData] = useState();
-  const [chartData, setChartData] = useState();
-  const [dataMax, setDataMax] = useState();
-  const [customTooltipData, setCustomTooltipData] = useState();
+const ChartPreview = ({indicator, chartNameField, chartType, tableColumns}) => {
+  const {selectedTable} = useSelectedTableValue()
+  const {getTokenSilently} = useAuth0()
+  const [responseData, setResponseData] = useState()
+  const [chartData, setChartData] = useState()
+  const [dataMax, setDataMax] = useState()
+  const [customTooltipData, setCustomTooltipData] = useState()
 
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
-  const isDesktopOrLaptop = useMediaQuery({ query: "(min-width: 1224px)" });
+  const isTabletOrMobile = useMediaQuery({query: '(max-width: 1224px)'})
+  const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'})
 
   useEffect(() => {
-    getData();
-  }, [indicator, chartNameField]);
+    getData()
+  }, [indicator, chartNameField])
 
   /* useEffect(() => {
     getData();
   }, [selectedTable, chartNameField, indicator, chartType]); */
 
   const getData = async () => {
-    const token = await getTokenSilently();
+    const token = await getTokenSilently()
 
     let res = await axios
-      .get("/admin/api/selectedtable", {
+      .get('/admin/api/selectedtable', {
         params: {
           tableName: selectedTable,
         },
@@ -58,29 +53,39 @@ const ChartPreview = ({
         },
       })
       .then((res) => {
-        setResponseData(res.data);
+        setResponseData(res.data)
 
-        let fixedDateData = parseDateColumn(res.data);
+        let fixedDateData = parseDateColumn(res.data)
         // console.log(fixedDateData)
 
         let chart = fixedDateData.map((val) => {
-          return {
-            name: val[chartNameField],
-            [indicator]: parseInt(val[indicator]),
-            year: val.Metai,
-          };
-        });
+          if (chartType != 'pieChart') {
+            return {
+              name: val[chartNameField],
+              [indicator]: parseInt(val[indicator]),
+              year: val.Metai,
+            }
+          } else {
+            let arr = indicator.map((indVal, index) => {
+              return {
+                name: indVal,
+                value: parseInt(val[indVal]),
+              }
+            })
+            return arr
+          }
+        })
 
-        setCustomTooltipData(fixedDateData.map((val) => val.Metai));
+        setCustomTooltipData(fixedDateData.map((val) => val.Metai))
 
         setDataMax(
-          Math.max(...fixedDateData.map((val) => val[indicator])) + 100
-        );
+          Math.max(...fixedDateData.map((val) => val[indicator])) + 100,
+        )
 
-        setChartData(chart);
+        setChartData(chart)
       })
-      .catch((err) => console.log(err));
-  };
+      .catch((err) => console.log(err))
+  }
 
   //we need to change format of our date column values, because we get YYYY-MM-DTH:m:s.SSSZ format
   // fixes date column from array of objects [{}{}{}..]
@@ -90,27 +95,27 @@ const ChartPreview = ({
         for (let i = 0; i < tableColumns.length; i++) {
           // console.log(row[tableColumns[i]]); // each object value
           if (
-            moment(row[tableColumns[i]], "YYYY-MM-DTH:m:s.SSSZ", true).isValid()
+            moment(row[tableColumns[i]], 'YYYY-MM-DTH:m:s.SSSZ', true).isValid()
           ) {
-            let objectValue = row[tableColumns[i]];
+            let objectValue = row[tableColumns[i]]
             // return moment(objectValue).format('YYYY/MM/DD')
             return {
               ...row,
-              [tableColumns[i]]: moment(objectValue).format("YYYY/MM/DD"),
-            };
+              [tableColumns[i]]: moment(objectValue).format('YYYY/MM/DD'),
+            }
           } else {
-            return { ...row };
+            return {...row}
           }
         }
-      });
-      return newResData;
+      })
+      return newResData
       // setResponseData(newResData);
     }
-  };
+  }
 
   return (
-    <Col span={16} style={{ display: "flex", flexWrap: "wrap" }}>
-      {chartType === "barChart" ? (
+    <Col span={16} style={{display: 'flex', flexWrap: 'wrap'}}>
+      {chartType === 'barChart' ? (
         <Barchart
           dataMax={dataMax}
           chartData={chartData}
@@ -118,7 +123,7 @@ const ChartPreview = ({
           chartNameField={chartNameField}
           customTooltipData={customTooltipData}
         />
-      ) : chartType === "columnChart" ? (
+      ) : chartType === 'columnChart' ? (
         <Columnchart
           dataMax={dataMax}
           chartData={chartData}
@@ -126,7 +131,7 @@ const ChartPreview = ({
           chartNameField={chartNameField}
           customTooltipData={customTooltipData}
         />
-      ) : chartData != null && chartType === "pieChart" ? (
+      ) : chartData != null && chartType === 'pieChart' ? (
         chartData.map((obj, index) => (
           <Piechart
             key={index}
@@ -137,7 +142,7 @@ const ChartPreview = ({
         ))
       ) : null}
     </Col>
-  );
-};
+  )
+}
 
-export default ChartPreview;
+export default ChartPreview
--- Client/src/components/CreateChart/Diagrams/Piechart.js
+++ Client/src/components/CreateChart/Diagrams/Piechart.js
@@ -1,39 +1,37 @@
-import React from "react";
+import React from 'react'
 
 //react-responsive
-import { useMediaQuery } from "react-responsive";
+import {useMediaQuery} from 'react-responsive'
 
 //recharts
-import { Tooltip, Legend, ResponsiveContainer, PieChart, Pie } from "recharts";
+import {Tooltip, Legend, ResponsiveContainer, PieChart, Pie} from 'recharts'
 
-const Piechart = ({ chartData, indicator, chartNameField }) => {
-  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 1224px)" });
-  const isDesktopOrLaptop = useMediaQuery({ query: "(min-width: 1224px)" });
+const Piechart = ({chartData, indicator, chartNameField}) => {
+  const isTabletOrMobile = useMediaQuery({query: '(max-width: 1224px)'})
+  const isDesktopOrLaptop = useMediaQuery({query: '(min-width: 1224px)'})
+
+  console.log(chartData)
 
   return (
     <>
       {indicator.length != 0 ? (
         <div>
-          {/* <ResponsiveContainer width={250} height={250}> */}
           <PieChart width={250} height={250}>
             <Pie
-              data={[chartData]}
-              dataKey={indicator[0]}
+              data={chartData}
+              dataKey="value"
               nameKey="name"
-              // cx="50%"
-              // cy="50%"
               outerRadius={80}
               fill="#1DA57A"
               isAnimationActive={false}
             />
             <Tooltip />
-            <Legend />
+            {/* <Legend /> */}
           </PieChart>
-          {/* </ResponsiveContainer> */}
         </div>
       ) : null}
     </>
-  );
-};
+  )
+}
 
-export default Piechart;
+export default Piechart
