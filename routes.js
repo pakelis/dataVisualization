b23007ee3dc9c003884739b23374d4dc5de9b92f
Mantas Pakulis <pakelis47@gmail.com>
2020-06-02 19:30:47 +0300

    initial commit

---
+++ routes.js
@@ -0,0 +1,80 @@
+var express = require('express')
+var router = express.Router()
+var db = require('./db')
+
+/* TEST CONNECTION  
+//the pool will emit and error on behalf of any idle clients
+//it contains if a backend error or network partition happens
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+pool.connect((err, client, done) => {
+  if (err) throw err;
+  client.query(
+    `SELECT * FROM siauliai_sportas.renginiu_finansavimas WHERE id = $1`,
+    [1],
+    (err, res) => {
+      done();
+
+      if (err) {
+        console.log(err);
+      } else {
+        console.log(res.rows[0]);
+      }
+    }
+  );
+});
+*/
+
+/*
+   ROUTES SECTION
+*/
+
+router.get('/api/hello', (req, res) => {
+  res.json('Hello from /api/hello')
+})
+
+//Get all table data from it's name
+router.get(`/api/get/table/`, (req, res, next) => {
+  const project_name = req.query.project_name
+
+  /* pool.query(`SELECT * FROM ${project_name}`, (q_err, q_res) => {
+    res.json(q_res.rows);
+  });
+   
+  this query made with pg 
+  */
+
+  db.any(`select * from ${project_name}`)
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+router.get('/api/get/tableone', (req, res, next) => {
+  const id = req.query.id
+
+  /* pool.query(
+    `SELECT * FROM renginiu_finansavimas where id=$1`,
+    [id],
+    (q_err, q_res) => {
+      res.json(q_res.rows);
+    }
+  );
+  */
+
+  db.any(`select * from renginiu_finansavimas where id=$1`, [id])
+    .then(rows => {
+      res.json(rows)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+})
+
+module.exports = router
